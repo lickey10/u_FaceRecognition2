4facf79112cd18951c3cdc67f156032edc9bcea1
lickey10 <lickey10@gmail.com>
2019-07-08 14:59:45 -0600

    first import

---
+++ FaceRecognition2/Assets/RealTimeFaceRecognitionExample/RealTimeFaceRecognitionExample.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+#if UNITY_5_3 || UNITY_5_3_OR_NEWER
+using UnityEngine.SceneManagement;
+#endif
+
+namespace RealTimeFaceRecognitionExample
+{
+    public class RealTimeFaceRecognitionExample : MonoBehaviour
+    {
+        // Use this for initialization
+        void Start ()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update ()
+        {
+
+        }
+
+        public void OnShowLicenseButtonClick ()
+        {
+            #if UNITY_5_3 || UNITY_5_3_OR_NEWER
+			SceneManager.LoadScene ("ShowLicense");
+            #else
+            Application.LoadLevel ("ShowLicense");
+            #endif
+        }
+
+        public void OnWebCamTextureRealTimeFaceRecognitionExampleButtonClick ()
+        {
+            #if UNITY_5_3 || UNITY_5_3_OR_NEWER
+            SceneManager.LoadScene ("WebCamTextureRealTimeFaceRecognitionExample");
+            #else
+            Application.LoadLevel ("WebCamTextureRealTimeFaceRecognitionExample");
+            #endif
+        }
+    }
+}
